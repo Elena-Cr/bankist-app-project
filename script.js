54f77cc2aa1e6680fced5486b9ed56a5e92dd7b5
Elena Cristescu <elenacristescu11@gmail.com>
2023-10-25 20:15:58 +0200

    Challenge 1

--- script.js
+++ script.js
@@ -176,3 +176,15 @@ currenciesUnique.forEach(function (value, key, map) {
   console.log(`${key}:${value}`);
 });
 */
+//CHALLENGE
+const checkDogs = function (dogsJulia, dogsKate) {
+  let dogsJuliaCorrect = [...dogsJulia].slice(1, -2);
+  let dogs = [...dogsJuliaCorrect, ...dogsKate];
+  dogs.forEach(function (age, index, array) {
+    const type = age > 3 ? `an adult, and is ${age} years old` : 'a puppy';
+    console.log(`Dog number ${index + 1} is ${type}`);
+  });
+};
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
