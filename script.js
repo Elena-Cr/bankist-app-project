0f09ded3645e53269e073aab0cf686b7da10c180
Elena Cristescu <elenacristescu11@gmail.com>
2023-11-03 19:34:14 +0100

    example of sort method on strings and numbers

--- script.js
+++ script.js
@@ -460,10 +460,39 @@ const deposit = mov => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
-*/
+
 
 //FLAT AND FLATMAP
 const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
 console.log(arr.flat());
 const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
 console.log(arr.flat());
+*/
+//SORT
+//Strings
+const owners = ['Elena', 'Zach', 'Adam', 'Martha'];
+console.log(owners.sort());
+console.log(owners);
+
+//Numbers
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// console.log(movements.sort());
+
+//return<0,A,B
+//return>0,B,A
+
+//Ascending
+// const numberAssc = movements.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (a < b) return -1;
+// });
+movements.sort((a, b) => a - b);
+console.log(movements);
+
+//Descending
+// movements.sort((a, b) => {
+//   if (a > b) return -1;
+//   if (a < b) return 1;
+// });
+movements.sort((a, b) => b - a);
+console.log(movements);
